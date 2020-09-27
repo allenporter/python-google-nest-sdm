37bb718c50c5c1aa444495addf7427ae3b443c38
Allen Porter <allen@thebends.org>
2020-09-27 19:34:54 +0000

    Add skeleton auth library and setup.
    This auth skeleton is the example provided in the HomeAssistant
    documentation: https://developers.home-assistant.io/docs/api_lib_auth

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup
+
+version = '0.0.1'
+
+setup(name='google-nest-sdm',
+      version=version,
+      description='Python API for talking to Google Nest using the SDM API',
+      keywords='nest',
+      author='Allen Porter',
+      author_email='allen.porter@gmail.com',
+      url='https://github.com/allenporter/python-google-nest-sdm',
+      packages=['google-nest-sdm'],
+      install_requires=[
+          'asyncio==3.4.3',
+          'aiohttp==3.6.2',
+      ])
