fc2be2f545b9b62942597088ad1275e9c6a6ea39
Allen Porter <allen@thebends.org>
2020-09-28 06:51:40 +0000

    Add more common device traits
    Add Connectivity, Humidity, and Temperature mixins that are populated based on
    present traits in the API response.

--- google_nest_sdm/device.py
+++ google_nest_sdm/device.py
@@ -2,6 +2,14 @@ from .auth import AbstractAuth
 
 from abc import abstractproperty, ABCMeta
 
+DEVICE_NAME = 'name'
+DEVICE_TYPE = 'type'
+DEVICE_TRAITS = 'traits'
+STATUS = 'status'
+CUSTOM_NAME = 'customName'
+AMBIENT_HUMIDITY_PERCENT = 'ambientHumidityPercent'
+AMBIENT_TEMPERATURE_CELSIUS = 'ambientTemperatureCelsius'
+
 # Every trait is implemented as a mixin that is attached to Device.  Each
 # mixin expects to be able to get its traits from the device itself.
 class WithTraits(object):
@@ -10,6 +18,22 @@ class WithTraits(object):
   @abstractproperty
   def _traits(self, trait) -> dict:
     pass
+
+
+class ConnectivityMixin:
+  """This trait belongs to any device that has connectivity information."""
+
+  NAME = 'sdm.devices.traits.Connectivity'
+
+  @property
+  def status(self) -> str:
+    """Device connectivity status.
+
+    Return:
+      "OFFLINE", "ONLINE"
+    """
+    data = self._traits(ConnectivityMixin.NAME)
+    return data[STATUS]
 
 
 class InfoMixin:
@@ -21,18 +45,41 @@ class InfoMixin:
   def custom_name(self) -> str:
     """Custom name of the device."""
     data = self._traits(InfoMixin.NAME)
-    return data['customName']
+    return data[CUSTOM_NAME]
+
+
+class HumidityMixin:
+  """This trait belongs to any device that has a sensor to measure humidity."""
+
+  NAME = 'sdm.devices.traits.Humidity'
+
+  @property
+  def ambient_humidity_percent(self) -> float:
+    """Percent humidity, measured at the device."""
+    data = self._traits(HumidityMixin.NAME)
+    return data[AMBIENT_HUMIDITY_PERCENT]
+
+
+class TemperatureMixin:
+  """This trait belongs to any device that has a sensor to measure temperature."""
+
+  NAME = 'sdm.devices.traits.Temperature'
+
+  @property
+  def ambient_temperature_celsius(self) -> float:
+    """Percent humidity, measured at the device."""
+    data = self._traits(TemperatureMixin.NAME)
+    return data[AMBIENT_TEMPERATURE_CELSIUS]
 
 
 # Create a map of all traits names to the class that can create it
 _DEVICE_TRAIT_MIXINS = [
+  ConnectivityMixin,
   InfoMixin,
+  HumidityMixin,
+  TemperatureMixin,
 ]
 _DEVICE_TRAIT_MAP = { cls.NAME: cls for cls in _DEVICE_TRAIT_MIXINS }
-
-_DEVICE_NAME = "name"
-_DEVICE_TYPE = "type"
-_DEVICE_TRAITS = "traits"
 
 
 class Device(WithTraits):
@@ -45,35 +92,35 @@ class Device(WithTraits):
 
   @staticmethod
   def MakeDevice(raw_data: dict, auth: AbstractAuth):
+    """Creates a device with the appropriate traits."""
     bases = [Device]
-    if _DEVICE_TRAITS in raw_data:
-      for (trait, data) in raw_data[_DEVICE_TRAITS].items():
+    if DEVICE_TRAITS in raw_data:
+      for (trait, data) in raw_data[DEVICE_TRAITS].items():
         if trait in _DEVICE_TRAIT_MAP:
           cls = _DEVICE_TRAIT_MAP[trait]
           bases.append(cls)
     d = Device(raw_data, auth)
     d.__class__ = type('DynamicDevice', tuple(bases), {})
     return d
-    
 
   @property
   def name(self) -> str:
     """Return the full name and device identifier for the device."""
-    return self._raw_data[_DEVICE_NAME] 
+    return self._raw_data[DEVICE_NAME] 
 
   @property
   def type(self) -> str:
-    return self._raw_data[_DEVICE_TYPE]
+    return self._raw_data[DEVICE_TYPE]
 
   @property
   def traits(self) -> list:
-    if not _DEVICE_TRAITS in self._raw_data:
+    if not DEVICE_TRAITS in self._raw_data:
       return {}
-    return list(self._raw_data[_DEVICE_TRAITS].keys())
+    return list(self._raw_data[DEVICE_TRAITS].keys())
 
   def _traits(self, trait) -> dict:
     """Return the raw dictionary for the specified trait."""
-    if (not _DEVICE_TRAITS in self._raw_data or
-        not trait in self._raw_data[_DEVICE_TRAITS]):
+    if (not DEVICE_TRAITS in self._raw_data or
+        not trait in self._raw_data[DEVICE_TRAITS]):
       return {}
-    return self._raw_data[_DEVICE_TRAITS][trait]
+    return self._raw_data[DEVICE_TRAITS][trait]
--- tests/device_test.py
+++ tests/device_test.py
@@ -25,6 +25,11 @@ class DeviceTest(unittest.TestCase):
     device = Device.MakeDevice(raw, auth=None)
     self.assertEqual("my/device/name", device.name)
     self.assertEqual("sdm.devices.types.SomeDeviceType", device.type)
+    self.assertFalse(hasattr(device, 'status'))
+    self.assertFalse(hasattr(device, 'custom_name'))
+    self.assertFalse(hasattr(device, 'ambient_humidity_percent'))
+    self.assertFalse(hasattr(device, 'ambient_temperature_celsius'))
+    self.assertFalse(hasattr(device, 'custom_name'))
 
   def testInfoTraits(self):
     raw = {
@@ -41,4 +46,62 @@ class DeviceTest(unittest.TestCase):
     self.assertEqual("sdm.devices.types.SomeDeviceType", device.type)
     self.assertEqual("Device Name", device.custom_name)
     self.assertEqual(["sdm.devices.traits.Info"], device.traits)
+
+  def testConnectivityTraits(self):
+    raw = {
+       "traits": {
+         "sdm.devices.traits.Connectivity": {
+           "status": "OFFLINE",
+         },
+       },
+    }
+    device = Device.MakeDevice(raw, auth=None)
+    self.assertEqual("OFFLINE", device.status)
+    self.assertEqual(["sdm.devices.traits.Connectivity"], device.traits)
+
+  def testHumidityTraits(self):
+    raw = {
+       "traits": {
+         "sdm.devices.traits.Humidity": {
+           "ambientHumidityPercent": "25.3",
+         },
+       },
+    }
+    device = Device.MakeDevice(raw, auth=None)
+    self.assertEqual("25.3", device.ambient_humidity_percent)
+    self.assertEqual(["sdm.devices.traits.Humidity"], device.traits)
+
+  def testTemperatureTraits(self):
+    raw = {
+       "traits": {
+         "sdm.devices.traits.Temperature": {
+           "ambientTemperatureCelsius": "31.1",
+         },
+       },
+    }
+    device = Device.MakeDevice(raw, auth=None)
+    self.assertEqual("31.1", device.ambient_temperature_celsius)
+    self.assertEqual(["sdm.devices.traits.Temperature"], device.traits)
+
+  def testMultipleTraits(self):
+    raw = {
+       "name": "my/device/name",
+       "type": "sdm.devices.types.SomeDeviceType",
+       "traits": {
+         "sdm.devices.traits.Info": {
+           "customName": "Device Name",
+         },
+         "sdm.devices.traits.Connectivity": {
+           "status": "OFFLINE",
+         },
+       },
+    }
+    device = Device.MakeDevice(raw, auth=None)
+    self.assertEqual("my/device/name", device.name)
+    self.assertEqual("sdm.devices.types.SomeDeviceType", device.type)
+    self.assertEqual("Device Name", device.custom_name)
+    self.assertEqual("OFFLINE", device.status)
+    self.assertEqual(["sdm.devices.traits.Info",
+                      "sdm.devices.traits.Connectivity"], device.traits)
+
 
