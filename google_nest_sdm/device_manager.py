1a3025770d21beac489f1f813a05cce3009bf719
Allen Porter <allen@thebends.org>
2021-07-18 18:03:50 -0700

    Fix lint error in complex method

--- google_nest_sdm/device_manager.py
+++ google_nest_sdm/device_manager.py
@@ -1,7 +1,7 @@
 """Device Manager keeps track of the current state of all devices."""
 
 from .device import Device
-from .event import EventMessage
+from .event import EventMessage, RelationUpdate
 from .structure import InfoTrait, RoomInfoTrait, Structure
 
 
@@ -24,37 +24,45 @@ class DeviceManager:
         """Return current state of structures."""
         return self._structures
 
-    def add_device(self, device: Device):
+    def add_device(self, device: Device) -> None:
         """Track the specified device."""
         self._devices[device.name] = device
 
-    def add_structure(self, structure: Structure):
+    def add_structure(self, structure: Structure) -> None:
         """Track the specified device."""
         self._structures[structure.name] = structure
 
-    async def async_handle_event(self, event_message: EventMessage):
+    async def async_handle_event(self, event_message: EventMessage) -> None:
         """Handle a new message received."""
         if event_message.relation_update:
-            relation = event_message.relation_update
-            if relation.object in self._devices:
-                device = self._devices[relation.object]
-                if relation.type == "DELETED":
-                    # Delete device from room/structure
-                    if relation.subject in device.parent_relations:
-                        del device.parent_relations[relation.subject]
+            self._handle_device_relation(event_message.relation_update)
 
-                # Device moved to a room
-                if relation.type == "UPDATED" or relation.type == "CREATED":
-                    name = "Unknown"
-                    if relation.subject in self._structures:
-                        structure = self._structures[relation.subject]
-                        for trait_name in [InfoTrait.NAME, RoomInfoTrait.NAME]:
-                            if trait_name in structure.traits:
-                                name = structure.traits[trait_name].custom_name
-                                continue
-                    device.parent_relations[relation.subject] = name
         if event_message.resource_update_name:
             device_id = event_message.resource_update_name
             if device_id in self._devices:
                 device = self._devices[device_id]
                 await device.async_handle_event(event_message)
+
+    def _structure_name(self, relation_subject: str) -> str:
+        if relation_subject in self._structures:
+            structure = self._structures[relation_subject]
+            for trait_name in [InfoTrait.NAME, RoomInfoTrait.NAME]:
+                if trait_name in structure.traits:
+                    return structure.traits[trait_name].custom_name
+        return "Unknown"
+
+    def _handle_device_relation(self, relation: RelationUpdate) -> None:
+        if relation.object not in self._devices:
+            return
+
+        device = self._devices[relation.object]
+        if relation.type == "DELETED":
+            # Delete device from room/structure
+            if relation.subject in device.parent_relations:
+                del device.parent_relations[relation.subject]
+
+        if relation.type == "UPDATED" or relation.type == "CREATED":
+            # Device moved to a room
+            device.parent_relations[relation.subject] = self._structure_name(
+                relation.subject
+            )
