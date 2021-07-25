e84f58f04fc2145d0ecd10864f44e37f7adf4124
Allen Porter <allen@thebends.org>
2021-07-25 00:22:53 -0700

    Use fake device test fixture for doorbell traits

--- tests/doorbell_traits_test.py
+++ tests/doorbell_traits_test.py
@@ -1,34 +1,37 @@
-from google_nest_sdm.device import Device
+"""Tests for doorbell traits."""
 
 
-def test_doorbell_chime():
-    raw = {
-        "name": "my/device/name",
-        "traits": {
-            "sdm.devices.traits.DoorbellChime": {},
-        },
-    }
-    device = Device.MakeDevice(raw, auth=None)
+def test_doorbell_chime(fake_device):
+    device = fake_device(
+        {
+            "name": "my/device/name",
+            "traits": {
+                "sdm.devices.traits.DoorbellChime": {},
+            },
+        }
+    )
     assert "sdm.devices.traits.DoorbellChime" in device.traits
 
 
-def test_doorbell_chime_trait_hack():
+def test_doorbell_chime_trait_hack(fake_device):
     """Adds the DoorbellChime trait even when missing from the API to fix an API bug."""
-    raw = {
-        "name": "my/device/name",
-        "type": "sdm.devices.types.DOORBELL",
-        "traits": {},
-    }
-    device = Device.MakeDevice(raw, auth=None)
+    device = fake_device(
+        {
+            "name": "my/device/name",
+            "type": "sdm.devices.types.DOORBELL",
+            "traits": {},
+        }
+    )
     assert "sdm.devices.traits.DoorbellChime" in device.traits
 
 
-def test_doorbell_chime_trait_hack_not_applied():
+def test_doorbell_chime_trait_hack_not_applied(fake_device):
     """The doorbell chime trait hack is not applied for other types."""
-    raw = {
-        "name": "my/device/name",
-        "type": "sdm.devices.types.CAMERA",
-        "traits": {},
-    }
-    device = Device.MakeDevice(raw, auth=None)
+    device = fake_device(
+        {
+            "name": "my/device/name",
+            "type": "sdm.devices.types.CAMERA",
+            "traits": {},
+        }
+    )
     assert "sdm.devices.traits.DoorbellChime" not in device.traits
