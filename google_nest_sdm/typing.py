85bc7a92c28858775acdcab2ea1e170cd88af6ee
Allen Porter <allen@thebends.org>
2021-07-25 12:31:20 -0700

    Enable more mypy checks and warnings
    Issue #9

--- google_nest_sdm/typing.py
+++ google_nest_sdm/typing.py
@@ -1,6 +1,6 @@
 """Libraries for helping with typing API responses."""
 
-from typing import Any, Optional, Type, TypeVar, cast
+from typing import Any, Optional, Type, TypeVar
 
 T = TypeVar("T")
 
@@ -8,7 +8,7 @@ T = TypeVar("T")
 def cast_assert(t: Type[T], data: Any) -> T:
     """Function to aid in extracting type values from API responses."""
     assert isinstance(data, t), f"Expected data with type {t} but was {type(data)}"
-    return cast(T, data)
+    return data
 
 
 def cast_optional(t: Type[T], data: Any) -> Optional[T]:
