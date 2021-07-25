ca702d96a6b6112dce439563aec0ad82fb0c5289
Allen Porter <allen@thebends.org>
2021-07-25 00:26:14 -0700

    Use fixtures for thermostat traits and device tests

--- tests/device_test.py
+++ tests/device_test.py
@@ -1,84 +1,91 @@
-from google_nest_sdm.device import Device
+"""Tests for device properties."""
 
 
-def test_device_id():
-    raw = {
-        "name": "my/device/name",
-        "type": "sdm.devices.types.SomeDeviceType",
-    }
-    device = Device.MakeDevice(raw, auth=None)
+def test_device_id(fake_device):
+    device = fake_device(
+        {
+            "name": "my/device/name",
+            "type": "sdm.devices.types.SomeDeviceType",
+        }
+    )
     assert "my/device/name" == device.name
     assert "sdm.devices.types.SomeDeviceType" == device.type
 
 
-def test_no_traits():
-    raw = {
-        "name": "my/device/name",
-    }
-    device = Device.MakeDevice(raw, auth=None)
+def test_no_traits(fake_device):
+    device = fake_device(
+        {
+            "name": "my/device/name",
+        }
+    )
     assert "my/device/name" == device.name
     assert not ("sdm.devices.traits.Info" in device.traits)
 
 
-def test_empty_traits():
-    raw = {
-        "name": "my/device/name",
-        "traits": {},
-    }
-    device = Device.MakeDevice(raw, auth=None)
+def test_empty_traits(fake_device):
+    device = fake_device(
+        {
+            "name": "my/device/name",
+            "traits": {},
+        }
+    )
     assert "my/device/name" == device.name
     assert not ("sdm.devices.traits.Info" in device.traits)
 
 
-def test_no_parent_relations():
-    raw = {
-        "name": "my/device/name",
-    }
-    device = Device.MakeDevice(raw, auth=None)
+def test_no_parent_relations(fake_device):
+    device = fake_device(
+        {
+            "name": "my/device/name",
+        }
+    )
     assert "my/device/name" == device.name
     assert {} == device.parent_relations
 
 
-def test_empty_parent_relations():
-    raw = {
-        "name": "my/device/name",
-        "parentRelations": [],
-    }
-    device = Device.MakeDevice(raw, auth=None)
+def test_empty_parent_relations(fake_device):
+    device = fake_device(
+        {
+            "name": "my/device/name",
+            "parentRelations": [],
+        }
+    )
     assert "my/device/name" == device.name
     assert {} == device.parent_relations
 
 
-def test_parent_relation():
-    raw = {
-        "name": "my/device/name",
-        "parentRelations": [
-            {
-                "parent": "my/structure/or/room",
-                "displayName": "Some Name",
-            },
-        ],
-    }
-    device = Device.MakeDevice(raw, auth=None)
+def test_parent_relation(fake_device):
+    device = fake_device(
+        {
+            "name": "my/device/name",
+            "parentRelations": [
+                {
+                    "parent": "my/structure/or/room",
+                    "displayName": "Some Name",
+                },
+            ],
+        }
+    )
     assert "my/device/name" == device.name
     assert {"my/structure/or/room": "Some Name"} == device.parent_relations
 
 
-def test_multiple_parent_relations():
-    raw = {
-        "name": "my/device/name",
-        "parentRelations": [
-            {
-                "parent": "my/structure/or/room1",
-                "displayName": "Some Name1",
-            },
-            {
-                "parent": "my/structure/or/room2",
-                "displayName": "Some Name2",
-            },
-        ],
-    }
-    device = Device.MakeDevice(raw, auth=None)
+def test_multiple_parent_relations(fake_device):
+    device = fake_device(
+        {
+            "name": "my/device/name",
+            "parentRelations": [
+                {
+                    "parent": "my/structure/or/room1",
+                    "displayName": "Some Name1",
+                },
+                {
+                    "parent": "my/structure/or/room2",
+                    "displayName": "Some Name2",
+                },
+            ],
+        }
+    )
     assert "my/device/name" == device.name
     assert {
         "my/structure/or/room1": "Some Name1",
--- tests/thermostat_traits_test.py
+++ tests/thermostat_traits_test.py
@@ -1,19 +1,20 @@
-from google_nest_sdm.device import Device
+"""Tests for thermostat traits."""
 
 
-def test_thermostat_eco_traits():
-    raw = {
-        "name": "my/device/name",
-        "traits": {
-            "sdm.devices.traits.ThermostatEco": {
-                "availableModes": ["MANUAL_ECHO", "OFF"],
-                "mode": "MANUAL_ECHO",
-                "heatCelsius": 20.0,
-                "coolCelsius": 22.0,
+def test_thermostat_eco_traits(fake_device):
+    device = fake_device(
+        {
+            "name": "my/device/name",
+            "traits": {
+                "sdm.devices.traits.ThermostatEco": {
+                    "availableModes": ["MANUAL_ECHO", "OFF"],
+                    "mode": "MANUAL_ECHO",
+                    "heatCelsius": 20.0,
+                    "coolCelsius": 22.0,
+                },
             },
-        },
-    }
-    device = Device.MakeDevice(raw, auth=None)
+        }
+    )
     assert "sdm.devices.traits.ThermostatEco" in device.traits
     trait = device.traits["sdm.devices.traits.ThermostatEco"]
     assert ["MANUAL_ECHO", "OFF"] == trait.available_modes
@@ -22,79 +23,83 @@ def test_thermostat_eco_traits():
     assert 22.0 == trait.cool_celsius
 
 
-def test_thermostat_hvac_traits():
-    raw = {
-        "name": "my/device/name",
-        "traits": {
-            "sdm.devices.traits.ThermostatHvac": {
-                "status": "HEATING",
+def test_thermostat_hvac_traits(fake_device):
+    device = fake_device(
+        {
+            "name": "my/device/name",
+            "traits": {
+                "sdm.devices.traits.ThermostatHvac": {
+                    "status": "HEATING",
+                },
             },
-        },
-    }
-    device = Device.MakeDevice(raw, auth=None)
+        }
+    )
     assert "sdm.devices.traits.ThermostatHvac" in device.traits
     trait = device.traits["sdm.devices.traits.ThermostatHvac"]
     assert "HEATING" == trait.status
 
 
-def test_thermostat_mode_traits():
-    raw = {
-        "name": "my/device/name",
-        "traits": {
-            "sdm.devices.traits.ThermostatMode": {
-                "availableModes": ["HEAT", "COOL", "HEATCOOL", "OFF"],
-                "mode": "COOL",
+def test_thermostat_mode_traits(fake_device):
+    device = fake_device(
+        {
+            "name": "my/device/name",
+            "traits": {
+                "sdm.devices.traits.ThermostatMode": {
+                    "availableModes": ["HEAT", "COOL", "HEATCOOL", "OFF"],
+                    "mode": "COOL",
+                },
             },
-        },
-    }
-    device = Device.MakeDevice(raw, auth=None)
+        }
+    )
     assert "sdm.devices.traits.ThermostatMode" in device.traits
     trait = device.traits["sdm.devices.traits.ThermostatMode"]
     assert ["HEAT", "COOL", "HEATCOOL", "OFF"] == trait.available_modes
     assert "COOL" == trait.mode
 
 
-def test_thermostat_temperature_setpoint_traits():
-    raw = {
-        "name": "my/device/name",
-        "traits": {
-            "sdm.devices.traits.ThermostatTemperatureSetpoint": {
-                "heatCelsius": 20.0,
-                "coolCelsius": 22.0,
+def test_thermostat_temperature_setpoint_traits(fake_device):
+    device = fake_device(
+        {
+            "name": "my/device/name",
+            "traits": {
+                "sdm.devices.traits.ThermostatTemperatureSetpoint": {
+                    "heatCelsius": 20.0,
+                    "coolCelsius": 22.0,
+                },
             },
-        },
-    }
-    device = Device.MakeDevice(raw, auth=None)
+        }
+    )
     assert "sdm.devices.traits.ThermostatTemperatureSetpoint" in device.traits
     trait = device.traits["sdm.devices.traits.ThermostatTemperatureSetpoint"]
     assert 20.0 == trait.heat_celsius
     assert 22.0 == trait.cool_celsius
 
 
-def test_thermostat_multiple_traits():
-    raw = {
-        "name": "my/device/name",
-        "traits": {
-            "sdm.devices.traits.ThermostatEco": {
-                "availableModes": ["MANUAL_ECHO", "OFF"],
-                "mode": "MANUAL_ECHO",
-                "heatCelsius": 21.0,
-                "coolCelsius": 22.0,
+def test_thermostat_multiple_traits(fake_device):
+    device = fake_device(
+        {
+            "name": "my/device/name",
+            "traits": {
+                "sdm.devices.traits.ThermostatEco": {
+                    "availableModes": ["MANUAL_ECHO", "OFF"],
+                    "mode": "MANUAL_ECHO",
+                    "heatCelsius": 21.0,
+                    "coolCelsius": 22.0,
+                },
+                "sdm.devices.traits.ThermostatHvac": {
+                    "status": "HEATING",
+                },
+                "sdm.devices.traits.ThermostatMode": {
+                    "availableModes": ["HEAT", "COOL", "HEATCOOL", "OFF"],
+                    "mode": "COOL",
+                },
+                "sdm.devices.traits.ThermostatTemperatureSetpoint": {
+                    "heatCelsius": 23.0,
+                    "coolCelsius": 24.0,
+                },
             },
-            "sdm.devices.traits.ThermostatHvac": {
-                "status": "HEATING",
-            },
-            "sdm.devices.traits.ThermostatMode": {
-                "availableModes": ["HEAT", "COOL", "HEATCOOL", "OFF"],
-                "mode": "COOL",
-            },
-            "sdm.devices.traits.ThermostatTemperatureSetpoint": {
-                "heatCelsius": 23.0,
-                "coolCelsius": 24.0,
-            },
-        },
-    }
-    device = Device.MakeDevice(raw, auth=None)
+        }
+    )
     assert "sdm.devices.traits.ThermostatEco" in device.traits
     assert "sdm.devices.traits.ThermostatHvac" in device.traits
     assert "sdm.devices.traits.ThermostatMode" in device.traits
