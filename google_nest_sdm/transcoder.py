71801010f8054067dcacc81e866c40c3c0779367
Allen Porter <allen.porter@gmail.com>
2022-01-14 21:13:23 -0800

    Support transcoding of clip previews mp4s to gif (#163)
    * Support transcoding of clip previews mp4s to gif
    
    * Remove dead code
    
    * Add test coverage for clip transcoding
    
    * Fix test coverag e for transcoder
    
    * Increase test coverage for failure cases
    
    * Increase test coverage for disabled state
    
    * Exercise more failure cases with additional coverage

---
+++ google_nest_sdm/transcoder.py
@@ -0,0 +1,47 @@
+"""Library for transcoding mp4 clips."""
+
+import asyncio.subprocess
+import logging
+import os
+
+from .exceptions import TranscodeException
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class Transcoder:
+    """A worker that processes mp4 clips."""
+
+    def __init__(self, ffmpeg_binary: str, path_prefix: str) -> None:
+        """Initialize transcoder."""
+        self._ffmpeg_binary = ffmpeg_binary
+        self._path_prefix = path_prefix
+
+    async def transcode_clip(self, input_file: str, output_file: str) -> None:
+        """Create a image preview for a thumbnail clip."""
+        full_input_file = f"{self._path_prefix}/{input_file}"
+        full_output_file = f"{self._path_prefix}/{output_file}"
+        if not os.path.exists(full_input_file):
+            raise TranscodeException(f"Input file does not exist: {full_input_file}")
+        if os.path.exists(full_output_file):
+            raise TranscodeException(f"Ouput file already exists: {full_output_file}")
+        cmd = " ".join(
+            [
+                self._ffmpeg_binary,
+                "-y",
+                "-i",
+                full_input_file,
+                "-vf setpts=2.0*PTS",
+                "-vf scale=320:-1,setsar=1:1",
+                "-r 4",
+                "-loop 0",
+                full_output_file,
+            ]
+        )
+        proc = await asyncio.create_subprocess_shell(cmd)
+        stdout, stderr = await proc.communicate()
+        if proc.returncode != 0:
+            _LOGGER.debug(stderr)
+            raise TranscodeException(
+                f"Transcode command failure: {cmd} code: {proc.returncode}"
+            )
---
+++ tests/test_transcoder.py
@@ -0,0 +1,57 @@
+from unittest.mock import Mock, patch
+
+import asynctest
+import pytest
+
+from google_nest_sdm.exceptions import TranscodeException
+from google_nest_sdm.transcoder import Transcoder
+
+BINARY = "/bin/true"
+
+
+async def test_transcoder_file_not_exist(tmp_path: str) -> None:
+    t = Transcoder(BINARY, path_prefix=tmp_path)
+    with pytest.raises(TranscodeException):
+        await t.transcode_clip("in_file.mp4", "out_file.gif")
+
+
+async def test_transcoder_output_already_exists(tmp_path: str) -> None:
+    t = Transcoder(BINARY, path_prefix=tmp_path)
+    with open(f"{tmp_path}/in_file.mp4", mode="w") as f:
+        f.write("some-input")
+    with open(f"{tmp_path}/out_file.gif", mode="w") as f:
+        f.write("some-output")
+    with pytest.raises(TranscodeException):
+        await t.transcode_clip("in_file.mp4", "out_file.gif")
+
+
+async def test_transcoder(tmp_path: str) -> None:
+    t = Transcoder(BINARY, path_prefix=tmp_path)
+    with open(f"{tmp_path}/in_file.mp4", mode="w") as f:
+        f.write("some-input")
+    with patch(
+        "google_nest_sdm.transcoder.asyncio.create_subprocess_shell"
+    ) as mock_shell:
+        process_mock = Mock()
+        future = asynctest.asyncio.Future()
+        future.set_result(("", ""))
+        process_mock.communicate.return_value = future
+        process_mock.returncode = 0
+        mock_shell.return_value = process_mock
+        await t.transcode_clip("in_file.mp4", "out_file.gif")
+
+
+async def test_transcoder_failure(tmp_path: str) -> None:
+    t = Transcoder("/bin/false", path_prefix=tmp_path)
+    with open(f"{tmp_path}/in_file.mp4", mode="w") as f:
+        f.write("some-input")
+    with patch(
+        "google_nest_sdm.transcoder.asyncio.create_subprocess_shell"
+    ) as mock_shell, pytest.raises(TranscodeException):
+        process_mock = Mock()
+        future = asynctest.asyncio.Future()
+        future.set_result(("", ""))
+        process_mock.communicate.return_value = future
+        process_mock.returncode = 1
+        mock_shell.return_value = process_mock
+        await t.transcode_clip("in_file.mp4", "out_file.gif")
