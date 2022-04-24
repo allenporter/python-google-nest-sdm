d5c6f6adcf6c32d4dc8c58a4006a2adfe0636cd5
Allen Porter <allen@thebends.org>
2022-04-24 16:33:47 -0700

    Require python3.9 as that is the only version tested

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         "requests-oauthlib>=1.3.0",
         "PyYAML==6.0",
     ],
+    python_requires=">=3.9",
     entry_points={
         "console_scripts": [
             "google_nest=google_nest_sdm.google_nest:main",
