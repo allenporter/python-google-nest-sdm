f3f9ecb599635714f84a29115fad3f0bec31c984
Allen Porter <allen@thebends.org>
2023-01-14 16:03:08 -0800

    Loosen PyYaml pin

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         "google-auth-oauthlib>=0.4.1",
         "google-cloud-pubsub>=2.1.0",
         "requests-oauthlib>=1.3.0",
-        "PyYAML==6.0",
+        "PyYAML>=6.0",
     ],
     python_requires=">=3.9",
     entry_points={
